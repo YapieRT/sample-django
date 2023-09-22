c31b58352c76dc888fcb8cca25f698f49d50dff6
Sokolov Valentyn <46300336+YapieRT@users.noreply.github.com>
2023-09-23 00:14:49 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 if os.getenv("DEVELOPMENT_MODE", "False") == "True":
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "django_db",
+            "USER": "db_user",
+            "PASSWORD": "password",
+            "HOST": "10.0.0.0",
+            "PORT": "5432",
         }
     }
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
