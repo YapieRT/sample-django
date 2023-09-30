0b6f2d555702767ada1a7564706b5ebf9264f822
Sokolov Valentyn <46300336+YapieRT@users.noreply.github.com>
2023-09-30 18:35:15 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 
 sleep 2
 python manage.py migrate
-python manage.py createsuperuser --username superpuperuser --noinput
+python manage.py createsuperuser --noinput
 python manage.py runserver 0.0.0.0:8000
